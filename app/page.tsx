12a03cc1dd4807b44db986fe3731e22d24c6d6c2
gangbeomsu <kbs21c1987@gmail.com>
2024-04-23 22:22:15 +0900

    notion api ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
+  fetch(
+    `https://api.notion.com/v1/databases/${process.env.NOTION_PORTFOLIO_DATABASE_ID}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NOTION_SECRET}`,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+
   return <main></main>;
 }
