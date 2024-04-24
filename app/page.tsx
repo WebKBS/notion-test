d0b143a676daa04dca318fd65cf2097a56656b19
gangbeomsu <kbs21c1987@gmail.com>
2024-04-24 22:13:00 +0900

    page 가져오기

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-export default function Home() {
-  fetch(
-    `https://api.notion.com/v1/databases/${process.env.NOTION_PORTFOLIO_DATABASE_ID}`,
+export default async function Home() {
+  const response = await fetch(
+    `https://api.notion.com/v1/pages/${process.env.NOTION_PORTFOLIO_DATABASE_ID}`,
     {
       headers: {
         Authorization: `Bearer ${process.env.NOTION_SECRET}`,
@@ -8,10 +8,19 @@ export default function Home() {
         'Content-Type': 'application/json',
       },
     }
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+  );
 
-  return <main></main>;
+  const data = await response.json();
+
+  console.log(data);
+  console.log(data.properties.image.files[0].file.url);
+  const location = data.properties['날짜'].date;
+  console.log(location);
+
+  const image = data.properties.image.files[0].file.url;
+  return (
+    <main>
+      <img src={image} alt="portfolio" width={200} height={200} />
+    </main>
+  );
 }
